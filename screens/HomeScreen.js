36f6f2ca5572cfb00e847036100be6678a072e90
AkshatMehta14 <4akshatmehta@gmail.com>
2023-10-13 16:01:29 -0400

    Basic Structure for landing page, did not add that much

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Dimensions } from 'react-native';
 import { auth } from '../FirebaseHandler';
 import {
   Image,
@@ -11,22 +11,36 @@ export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-
-    }
+    this.state = {};
   }
-  signOut = () => {
-    auth.signOut()
-    this.props.navigation.navigate("Login")
 
+  signOut = () => {
+    auth.signOut();
+    this.props.navigation.navigate("Login");
   }
 
   render() {
     return (
       <SafeAreaView style={styles.login}>
-
         <View style={styles.container}>
-
+          <View style={styles.textContainer}>
+            <Image style={styles.image} source={require("../assets/logo.png")} />
+            <Text style={styles.t2}>SafeZone</Text>
+            <Text style={styles.t}>Empowering Voices, Heating up Equality</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <View style={styles.buttonRow}>
+              <TouchableOpacity style={styles.halfSizeButton} onPress={() => { /* Handle button 1 action */ }}>
+                <Text style={styles.buttonText}>Heat Map</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.halfSizeButton} onPress={() => { /* Handle button 2 action */ }}>
+                <Text style={styles.buttonText}>Report</Text>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.halfSizeButtonCentered} onPress={() => { /* Handle button 3 action */ }}>
+              <Text style={styles.buttonText}>About Us</Text>
+            </TouchableOpacity>
+          </View>
           <TouchableOpacity style={styles.loginBtn} onPress={() => {
             this.signOut();
           }}>
@@ -37,7 +51,6 @@ export default class HomeScreen extends Component {
       </SafeAreaView>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -66,22 +79,71 @@ const styles = StyleSheet.create({
 
 
   },
-
-
-
-  t: {
+  t2: {
     fontSize: 35,
     fontWeight: '600',
   },
+
+
+  t: {
+    fontSize: 15,
+    fontWeight: '600',
+  },
+
 
   L2Login: {
     flex: 0.35,
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
+  textContainer: {
+    alignItems: 'center',
+    marginTop: 10, // Adjust the top margin to move text closer to the image
+  },
+  buttonContainer: {
+    alignItems: "center",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 15, // Adjust this margin as needed
+  },
+  halfSizeButton: {
+    width: "40%", // Half the size of the "Sign Out" button
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#08C91C",
+    marginHorizontal: 5, // Adjust this spacing as needed
+  },
+  halfSizeButtonCentered: {
+    width: "40%", // Half the size of the "Sign Out" button
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#08C91C",
+    marginBottom: 15, // Adjust this margin as needed
+  },
+  button: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#08C91C",
+    marginBottom: 15, // Adjust this margin as needed
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
   image: {
     resizeMode: 'contain',
-    marginBottom: 10,
+    marginBottom: 5,  // Reduce the margin to 5 units
+    width: Dimensions.get('window').width * 0.4,
+    height: Dimensions.get('window').height * 0.2,
     flex: 0.6,
   },
 
