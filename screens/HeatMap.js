722701e2b46daa74f68118e15dfeb376bda2e420
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2023-10-28 13:57:39 -0400

    Added functionality to filter categories when in maps, very buggy tho

--- screens/HeatMap.js
+++ screens/HeatMap.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Dimensions, Modal, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, SafeAreaView, Dimensions, Modal, ActivityIndicator, StatusBar } from 'react-native';
 import MapView, { Heatmap, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
-import firebase from 'firebase/app';
 import 'firebase/firestore';
-import axios from 'axios';
 import FirebaseInfo from '../FirebaseHandler';
 import { getDocs, collection } from 'firebase/firestore'; // Import the required Firestore functions
 import Icon from 'react-native-vector-icons/AntDesign';
@@ -18,14 +16,19 @@ export default class HeatMap extends Component {
 
     this.state = {
       location: null,
-      isKeyPageVisible: false,
       heatmapData: [], // Store heatmap data here
+      heatmapDataFull: [],
       isMenuVisible: false,
       heatmapRadius: 370,
       reportData: [], // Initial radius of the heatmap,
+      reportDataFull: [],
       infoVisible: false,
       info: ["", "", ""],
-      outOfZoom: false
+      outOfZoom: false,
+      sexism: true,
+      racism: true,
+      assault: true,
+      other: true,
     };
   }
 
@@ -38,7 +41,7 @@ export default class HeatMap extends Component {
     return { latitude: a[0].latitude, longitude: a[0].longitude };
   }
 
-
+  
   async componentDidMount() {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -54,18 +57,30 @@ export default class HeatMap extends Component {
 
 
     // Listen for changes in Firestore data
-    const querySnapshot = await getDocs(collection(FirebaseInfo.db, 'Reports2'));
-    querySnapshot.docs.map(async (doc) => {
-      const data = doc.data();
-      // Parse the address to latitude and longitude
-      const latLng = await this.parseAddressToLatLng(data.Address);
-      if (latLng) {
-        this.setState({
-          heatmapData: [...this.state.heatmapData, { coordinate: latLng, category: data.Category }],
-          reportData: [...this.state.reportData, { data: data, coords: latLng }]
-        });
-      }
-    });
+    await getDocs(collection(FirebaseInfo.db, 'Reports2')).then((data) => {
+
+      data.docs.map(async (doc) => {
+        const data = doc.data();
+        console.log(data)
+        // Parse the address to latitude and longitude
+        const latLng = await this.parseAddressToLatLng(data.Address);
+        console.log(latLng)
+
+        if (latLng) {
+          this.setState({
+            heatmapDataFull: [...this.state.heatmapDataFull, { latitude: latLng.latitude, longitude: latLng.longitude, weight: data.Intensity, category: data.Category }],
+            reportDataFull: [...this.state.reportDataFull, { data: data, coords: latLng, category: data.Category, intensity: data.Intensity }],
+            
+          });
+          this.setState({
+            heatmapData: this.state.heatmapDataFull,
+            reportData: this.state.reportDataFull
+          })
+        }
+      })
+
+    })
+    
   }
 
 
@@ -90,13 +105,23 @@ export default class HeatMap extends Component {
 
   toggleMenuPage = () => {
     this.setState({ isMenuVisible: !this.state.isMenuVisible });
-    console.log(this.state.heatmapData)
+    console.log(this.state.heatmapDataFull)
   }
 
+  changeHeatMapData = () => {
+    this.setState({heatmapData: this.state.heatmapDataFull.filter((data) => {
+      return !((!this.state.assault && data.category === 'assault') || (!this.state.sexism && data.category === 'sexism') ||
+      (!this.state.racism && data.category === 'racism') || (!this.state.other && data.category === 'other'))
+    }),
 
-  toggleKeyPage = () => {
-    this.setState({ isKeyPageVisible: !this.state.isKeyPageVisible });
+      reportData: this.state.reportDataFull.filter((data) => {
+        return !((!this.state.assault && data.category === 'assault') || (!this.state.sexism && data.category === 'sexism') ||
+        (!this.state.racism && data.category === 'racism') || (!this.state.other && data.category === 'other'))
+      })
+    })
   }
+
+  
 
 
   render() {
@@ -112,6 +137,7 @@ export default class HeatMap extends Component {
     return (
       <SafeAreaView style={styles.login}>
         <MapView
+          key={this.state.reload}
           provider={PROVIDER_GOOGLE}
           style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center' }}
           initialRegion={{
@@ -126,24 +152,25 @@ export default class HeatMap extends Component {
             <Marker
               key={key}
               coordinate={data.coords}
-              onPress={() => { this.setState({ info: [data.data.Address, data.data.Email, data.data.Description], infoVisible: true }) }}
+              onPress={() => { this.setState({ info: [data.data.Address, data.data.Email, data.data.Description, data.category, data.intensity], infoVisible: true }) }}
             />
           )) : null}
 
 
-          {!this.state.outOfZoom ? this.state.heatmapData.map((data, key) => (
-            <Heatmap
-              key={key}
-              points={[data.coordinate]}
-              opacity={0.7}
-              radius={this.state.heatmapRadius}
-              gradient={data.category === 'racism' ? { colors: ['blue', 'green', 'yellow', 'red'], startPoints: [0.25, 0.5, 0.75, 1], colorMapSize: 300 } :
-                data.category === 'assault' ? { colors: ['red', 'yellow', 'blue'], startPoints: [0.25, 0.5, 1], colorMapSize: 300 } :
-                  data.category === 'sexism' ? { colors: ['pink', 'purple'], startPoints: [0.5, 1], colorMapSize: 300 } :
-                    data.category === 'bullying' ? { colors: ['orange', 'red'], startPoints: [0.5, 1], colorMapSize: 300 } :
-                      data.category === 'other' ? { colors: ['gray', 'white'], startPoints: [0.5, 1], colorMapSize: 300 } : null}
-            />
-          )) : null}
+          {!this.state.outOfZoom ?
+           <Heatmap
+           points={this.state.heatmapData}
+           radius={this.state.heatmapRadius} // Adjust the radius
+           opacity={0.7}
+           gradient={{
+             colors: ['blue', 'green', 'yellow', 'red'],
+             startPoints: [0.25, 0.5, 0.75, 1],
+             colorMapSize: 300,
+           }}
+           
+         />
+           : null
+          } 
         </MapView>
 
 
@@ -153,6 +180,7 @@ export default class HeatMap extends Component {
               <View style={{ flexDirection: 'column', marginTop: 10, left: 12 }}>
                 <Text style={styles.address}>{this.state.info[0]}</Text>
                 <Text style={styles.email} >{this.state.info[1]}</Text>
+                <Text style={styles.email}>{this.state.info[3]} - {this.state.info[4]}</Text>
               </View>
               <TouchableOpacity onPress={() => { this.setState({ infoVisible: false }) }}><Icon name={'close'} color={'white'} size={33} style={{ right: 3, bottom: 3 }} /></TouchableOpacity>
             </View>
@@ -168,60 +196,105 @@ export default class HeatMap extends Component {
 
 
         <Modal transparent={true} animationType="slide" visible={this.state.isMenuVisible}>
-          <View style={styles.keyPage}>
-            <TouchableOpacity
-              style={styles.closeMenuButton}
-              onPress={this.toggleKeyPage}
-            >
-              <Text style={styles.buttonText}>Key</Text>
-            </TouchableOpacity>
-
-
-            <Modal
-              transparent={true}
-              animationType="slide"
-              visible={this.state.isKeyPageVisible}
-            >
-              <View style={styles.keyPage}>
-                <Text style={styles.keyPageText}>Color Key</Text>
-                <Text style={styles.normalText}>Racism: Color1</Text>
-                <Text style={styles.normalText}>Assault: Color2</Text>
-                <Text style={styles.normalText}>Sexism: Color3</Text>
-                <Text style={styles.normalText}>Bullying: Color4</Text>
-                <Text style={styles.normalText}>Extraneous: Color5</Text>
+          <SafeAreaView style={styles.keyPage}>
+            <View style={styles.buttonView}>
                 <TouchableOpacity
-                  style={styles.closeKeyButton}
-                  onPress={this.toggleKeyPage}
+                  style={styles.closeMenuButton}
+                  onPress={this.report}
+                >
+                  <Text style={styles.buttonText}>Report</Text>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity
+                  style={styles.closeMenuButton}
+                  onPress={this.exit}
+                >
+                  <Text style={styles.buttonText}>Home</Text>
+                </TouchableOpacity>
+
+
+                <TouchableOpacity
+                  style={styles.closeMenuButton}
+                  onPress={() => {this.toggleMenuPage() 
+                    this.changeHeatMapData()}}
                 >
                   <Text style={styles.buttonText}>Close</Text>
                 </TouchableOpacity>
-              </View>
-            </Modal>
+            </View>
+
+            <View style={styles.keyView}> 
+                <View style={styles.keyRow}>
+                     <TouchableOpacity
+                      style={{
+                        backgroundColor: this.state.assault ? "#08C91C" : 'red',
+                        borderRadius: 10,
+                        alignItems: "center",
+                        width: "45%",
+                        height: 60,
+                        justifyContent: 'center'
+                      }}
+                      onPress={() => this.setState({assault: !this.state.assault})}
+                    >
+                      <Text style={styles.keyButtonText}>Assault</Text>
+                    </TouchableOpacity>
 
 
-            <TouchableOpacity
-              style={styles.closeMenuButton}
-              onPress={this.report}
-            >
-              <Text style={styles.buttonText}>Report</Text>
-            </TouchableOpacity>
+                    <TouchableOpacity
+                      style={{
+                        backgroundColor: this.state.racism ? "#08C91C" : 'red',
+                        borderRadius: 10,
+                        alignItems: "center",
+                        width: "45%",
+                        height: 60,
+                        justifyContent: 'center'
+                      }}
+                      onPress={() => this.setState({racism: !this.state.racism})}
+                    >
+                      <Text style={styles.keyButtonText}>Racism</Text>
+                    </TouchableOpacity>
+
+                </View>
+                  
+
+                <View style={styles.keyRow}>
+                     <TouchableOpacity
+                      style={{
+                        backgroundColor: this.state.sexism ? '#08C91C' : 'red',
+                        borderRadius: 10,
+                        alignItems: "center",
+                        width: "45%",
+                        height: 60,
+                        justifyContent: 'center'
+                      }}
+                      onPress={() => this.setState({sexism: !this.state.sexism})}
+                    >
+                      <Text style={styles.keyButtonText}>Sexism</Text>
+                    </TouchableOpacity>
 
 
-            <TouchableOpacity
-              style={styles.closeMenuButton}
-              onPress={this.exit}
-            >
-              <Text style={styles.buttonText}>Return to Home</Text>
-            </TouchableOpacity>
+                    <TouchableOpacity
+                      style={{
+                        backgroundColor: this.state.other ? "#08C91C" : 'red',
+                        borderRadius: 10,
+                        alignItems: "center",
+                        width: "45%",
+                        height: 60,
+                        justifyContent: 'center'
+
+                      }}
+                      onPress={() => this.setState({other: !this.state.other})}
+                    >
+                      <Text style={styles.keyButtonText}>Other</Text>
+                    </TouchableOpacity>
+
+                </View>
+            </View>
 
 
-            <TouchableOpacity
-              style={styles.closeMenuButton}
-              onPress={this.toggleMenuPage}
-            >
-              <Text style={styles.buttonText}>Close</Text>
-            </TouchableOpacity>
-          </View>
+
+
+          </SafeAreaView>
         </Modal>
       </SafeAreaView>
     );
@@ -232,13 +305,26 @@ export default class HeatMap extends Component {
 
 
 const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+  keyButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  keyRow: {
+    flexDirection: 'row', 
+    justifyContent: 'space-around', 
+    width: '100%',
+    marginTop: 15
+  },
   closeMenuButton: {
     backgroundColor: "#08C91C",
     borderRadius: 25,
     alignItems: "center",
-    width: "40%",
+    width: "30%",
     padding: 10,
-    marginBottom: 10,
   },
   // Existing styles...
   line: {
@@ -288,14 +374,23 @@ const styles = StyleSheet.create({
     marginTop: 50,
     right: 5
   },
+  keyView: {
+    flex: 0.4,
+    top: Dimensions.get('screen').height * 0.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-
-
+  buttonView: {
+    top: 10,
+    flexDirection: 'row',
+    flex: 0.06,
+    justifyContent: 'space-around'
+  },
 
   keyPage: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
 
@@ -424,10 +519,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#08C91C",
     marginBottom: 15, // Adjust this margin as needed
   },
-  buttonText: {
-    fontSize: 17,
-    fontWeight: 'bold',
-  },
+  
   image: {
     resizeMode: 'contain',
     marginBottom: 5, // Reduce the margin to 5 units
